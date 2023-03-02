2d6f1248a0265693ddc31fc150e6141ddb6d4d89
lincon-programmer <lincongallo@gmail.com>
2023-03-02 13:22:40 -0300

    feat: Agora ao escolher uma imagem sai dela e selecionar outra, no perfil de origem, o botao de download ressurge novamente

--- src/main.js
+++ src/main.js
@@ -1,14 +1,33 @@
 function adicionarBotaoUI(){
     try{
         setTimeout(function(){
-            document.getElementsByClassName('_aamu _ae3_ _ae47 _ae48')[0].append(button_download);
+            var menu_reacoes = document.getElementsByClassName('_aamu _ae3_ _ae47 _ae48')[0];
+            if(menu_reacoes != undefined){
+                menu_reacoes.append(button_download)
+            };
         }, 2000);
     }catch(Ex){
         adicionarBotaoUI();
     }
 }
- 
-adicionarBotaoUI();
+
+function carregar_pagina() {
+    try {
+        setTimeout(function(){
+            var elements = document.querySelectorAll('._aabd');
+            elements.forEach(function(element){
+                
+                element.addEventListener('click', function(){
+                    adicionarBotaoUI();
+                });
+            });
+        }, 2000);
+    } catch (error) {
+        carregar_pagina();
+    }
+}
+
+carregar_pagina();
 
 button_download.addEventListener('click', () => {
     var link_image = document.getElementsByClassName('_aagu _aato')[0].firstChild.firstChild.src;
